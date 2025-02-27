e2df5fbf144326ba82405018a7efefddb25e0147
mInaciods <168947048+mInaciods@users.noreply.github.com>
2025-02-26 22:41:38 -0300

    Add files via upload

---
+++ Atividade 01/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matheus
+ * Date: 23/02/2025
+ * Time: 20:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Atividade_01
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btnMensagem;
+		private System.Windows.Forms.Label lblMensagem;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnMensagem = new System.Windows.Forms.Button();
+			this.lblMensagem = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnMensagem
+			// 
+			this.btnMensagem.Location = new System.Drawing.Point(101, 158);
+			this.btnMensagem.Name = "btnMensagem";
+			this.btnMensagem.Size = new System.Drawing.Size(75, 23);
+			this.btnMensagem.TabIndex = 0;
+			this.btnMensagem.Text = "Clique Aqui";
+			this.btnMensagem.UseVisualStyleBackColor = true;
+			this.btnMensagem.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// lblMensagem
+			// 
+			this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblMensagem.Location = new System.Drawing.Point(85, 74);
+			this.lblMensagem.Name = "lblMensagem";
+			this.lblMensagem.Size = new System.Drawing.Size(100, 23);
+			this.lblMensagem.TabIndex = 1;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.lblMensagem);
+			this.Controls.Add(this.btnMensagem);
+			this.Name = "MainForm";
+			this.Text = "Atividade 01";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Atividade 01/MainForm.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matheus
+ * Date: 23/02/2025
+ * Time: 20:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Atividade_01
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			lblMensagem.Text = "Olá, Mundo";
+		}
+	}
+}
